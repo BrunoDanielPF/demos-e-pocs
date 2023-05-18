40097892d06a191f9bd2cae235579edd77ee142e
Bruno Daniel <frandalozzo@gmail.com>
2023-05-17 21:35:08 -0300

    adicionando projeto basico de arduino

--- arduino/SensorUltrasonicoSimplesSerial/SensorUltrasonicoSimplesSerial.ino
+++ arduino/SensorUltrasonicoSimplesSerial/SensorUltrasonicoSimplesSerial.ino
@@ -1,37 +1,52 @@
-#include "Ultrasonic.h" //INCLUSÃO DA BIBLIOTECA NECESSÁRIA PARA FUNCIONAMENTO DO CÓDIGO
+#include "Ultrasonic.h"  //INCLUSÃO DA BIBLIOTECA NECESSÁRIA PARA FUNCIONAMENTO DO CÓDIGO
+#define som 9
+#define LED_RED_PIN 11
+#define LED_GREEN_PIN 12
 
-const int echoPin = 7; //PINO DIGITAL UTILIZADO PELO HC-SR04 ECHO(RECEBE)
-const int trigPin = 6; //PINO DIGITAL UTILIZADO PELO HC-SR04 TRIG(ENVIA)
+const int echoPin = 7;  //PINO DIGITAL UTILIZADO PELO HC-SR04 ECHO(RECEBE)
+const int trigPin = 6;  //PINO DIGITAL UTILIZADO PELO HC-SR04 TRIG(ENVIA)
 
-Ultrasonic ultrasonic(trigPin,echoPin); //INICIALIZANDO OS PINOS DO ARDUINO
+Ultrasonic ultrasonic(trigPin, echoPin);
 
-int distancia; //VARIÁVEL DO TIPO INTEIRO
-String result; //VARIÁVEL DO TIPO STRING
+int distancia;
+String result;
 
-void setup(){
-  pinMode(echoPin, INPUT); //DEFINE O PINO COMO ENTRADA (RECEBE)
-  pinMode(trigPin, OUTPUT); //DEFINE O PINO COMO SAIDA (ENVIA)
-  Serial.begin(9600); //INICIALIZA A PORTA SERIAL
-  }
-void loop(){
-  
-  hcsr04(); // FAZ A CHAMADA DO MÉTODO "hcsr04()"
-  Serial.print("Distancia "); //IMPRIME O TEXTO NO MONITOR SERIAL
-  Serial.print(result); ////IMPRIME NO MONITOR SERIAL A DISTÂNCIA MEDIDA
-  Serial.println("cm"); //IMPRIME O TEXTO NO MONITOR SERIAL
-  
+void setup() {
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+
+  hcsr04();
+  Serial.print("Distancia ");
+  Serial.print(result);
+  Serial.println("cm");
+  validateDistanceForSound(distancia);
 }
 //MÉTODO RESPONSÁVEL POR CALCULAR A DISTÂNCIA
-void hcsr04(){
-    digitalWrite(trigPin, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
-    delayMicroseconds(2); //INTERVALO DE 2 MICROSSEGUNDOS
-    digitalWrite(trigPin, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
-    delayMicroseconds(10); //INTERVALO DE 10 MICROSSEGUNDOS
-    digitalWrite(trigPin, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
-    //FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
-    //RESPOSTA DO ECHO EM CENTIMETROS, E ARMAZENA
-    //NA VARIAVEL "distancia"
-    distancia = (ultrasonic.read(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
-    result = String(distancia); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
-    delay(500); //INTERVALO DE 500 MILISSEGUNDOS
- }
+void hcsr04() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  distancia = (ultrasonic.read(CM));
+  result = String(distancia);
+  delay(500);
+}
+
+void validateDistanceForSound(int distancia) {
+  if (distancia < 5) {
+    tone(som, 600, 1500);
+    digitalWrite(LED_RED_PIN, HIGH);
+    digitalWrite(LED_GREEN_PIN, LOW);
+  } else {
+    tone(som, 0, 1500);
+    digitalWrite(LED_RED_PIN, LOW);
+    digitalWrite(LED_GREEN_PIN, HIGH);
+  }
+}
