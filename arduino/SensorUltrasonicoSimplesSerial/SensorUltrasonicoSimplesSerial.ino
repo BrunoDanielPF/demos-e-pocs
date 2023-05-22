3bd974958d27d9cc195cbb619bda530094665255
Bruno Daniel <frandalozzo@gmail.com>
2023-05-22 20:44:53 -0300

    atualizando projeto com sensor ultrasonico

--- arduino/SensorUltrasonicoSimplesSerial/SensorUltrasonicoSimplesSerial.ino
+++ arduino/SensorUltrasonicoSimplesSerial/SensorUltrasonicoSimplesSerial.ino
@@ -1,7 +1,9 @@
 #include "Ultrasonic.h"  //INCLUSÃO DA BIBLIOTECA NECESSÁRIA PARA FUNCIONAMENTO DO CÓDIGO
 #define som 9
-#define LED_RED_PIN 11 // PINO DIGITAL UTILIZADO PELO LED VERMELHO
-#define LED_GREEN_PIN 12 // PINO DIGITAL UTILIZADO PELO LED VERDE
+#define LED_RED_PIN 11
+#define LED_GREEN_PIN 12
+#define LED_RED_2_PIN 3
+#define LED_RED_3_PIN 5
 
 const int echoPin = 7;  //PINO DIGITAL UTILIZADO PELO HC-SR04 ECHO(RECEBE)
 const int trigPin = 6;  //PINO DIGITAL UTILIZADO PELO HC-SR04 TRIG(ENVIA)
@@ -15,10 +17,13 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_RED_2_PIN, OUTPUT);
+  pinMode(LED_RED_3_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
+
   hcsr04();
   Serial.print("Distancia ");
   Serial.print(result);
@@ -39,15 +44,30 @@ void hcsr04() {
 }
 
 void validateDistanceForSound(int distancia) {
-  //SE A DISTANCIA FOR MENOR QUE 5 CM IRA APITAR E LIGAR A LUZ VERMELHA
-  if (distancia < 5) {
-    tone(som, 600, 1500);
+  if (distancia < 10) {
+    tone(som, 1500, 1500);
     digitalWrite(LED_RED_PIN, HIGH);
+    digitalWrite(LED_RED_2_PIN, HIGH);
+    digitalWrite(LED_RED_3_PIN, HIGH);
+    digitalWrite(LED_GREEN_PIN, LOW);
+  }else if(distancia < 15){
+    digitalWrite(LED_RED_PIN, HIGH);
+    digitalWrite(LED_RED_2_PIN, HIGH);
+    digitalWrite(LED_RED_3_PIN, LOW);
+    digitalWrite(LED_GREEN_PIN, LOW);
+  }else if(distancia < 20) {
+    digitalWrite(LED_RED_PIN, HIGH);
+    digitalWrite(LED_RED_2_PIN, LOW);
+    digitalWrite(LED_RED_3_PIN, LOW);
     digitalWrite(LED_GREEN_PIN, LOW);
   } else {
-    //SE NAO LIGAR A LUZ VERDE
     tone(som, 0, 1500);
     digitalWrite(LED_RED_PIN, LOW);
+    digitalWrite(LED_RED_2_PIN, LOW);
+    digitalWrite(LED_RED_3_PIN, LOW);
+    digitalWrite(LED_GREEN_PIN, LOW);
+    delay(500);
     digitalWrite(LED_GREEN_PIN, HIGH);
+    delay(500);
   }
 }
